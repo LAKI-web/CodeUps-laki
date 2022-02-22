2ad811aa7f5b566a3b4752ffbad5cfe682fd6351
laki <maua.se.laki07@gmail.com>
2022-02-22 10:50:41 +0900

    2022_02_22morning

--- page-contact.php
+++ page-contact.php
@@ -18,32 +18,8 @@
 
 <div class="l-margin--contact-form"></div>
 <div class="contact-form">
-  <div class="section-inner">
-    <div class="contact-form__items">
-      <div class="contact-form__item">
-        <p class="contact-form__item-label">※会社名</p>
-        <input type="text" class="contact-form__text" placeholder="テキストがはいります" />
-      </div>
-      <div class="contact-form__item">
-        <p class="contact-form__item-label">※お名前</p>
-        <input type="text" class="contact-form__text" placeholder="テキストがはいります" />
-      </div>
-      <div class="contact-form__item">
-        <p class="contact-form__item-label">※ふりがな</p>
-        <input type="text" class="contact-form__text" placeholder="テキストがはいります" />
-      </div>
-      <div class="contact-form__item">
-        <p class="contact-form__item-label">※メールアドレス</p>
-        <input type="text" class="contact-form__text" placeholder="テキストがはいります" />
-      </div>
-      <div class="contact-form__item">
-        <p class="contact-form__item-label">※お問い合わせ内容</p>
-        <textarea class="contact-form__textarea" placeholder="テキストがはいります"></textarea>
-      </div>
-      <div class="contact-form__submit-btn">
-        <input class="contact-form__submit" type="submit" value="送信" />
-      </div>
-    </div>
+  <div class="contact-form__inner">
+    <?php echo do_shortcode('[contact-form-7 id="363" title="お問い合わせ"]'); ?>
     <!-- /.contact-form__items -->
   </div>
   <!-- /.section-inner -->
